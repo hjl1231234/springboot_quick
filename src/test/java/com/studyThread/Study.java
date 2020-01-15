64b01433b38f6de50c52e0b5bb701db5d09a46cd
hjltest <343323692@qq.com>
2020-01-15 21:10:43 +0800

    update design pattern

---
+++ src/test/java/com/studyThread/Study.java
@@ -0,0 +1,77 @@
+package com.studyThread;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class Study {
+    @Test
+    public void testCallable() throws ExecutionException, InterruptedException {
+        MyCallable myCallable = new MyCallable();
+        FutureTask<Integer> futureTask = new FutureTask<>(myCallable);
+        new Thread(futureTask).start();
+
+        Thread.sleep(1);
+        System.out.println("main thread    ");
+        futureTask.get();
+
+    }
+
+    @Test
+    public void testInterrupt() {
+//        Thread thread1 = new MyThread1();
+//        thread1.start();
+//        thread1.interrupt();
+//        System.out.println("Main run");
+
+        Thread thread2=new MyThread2();
+        thread2.start();
+        thread2.interrupt();
+
+        System.out.println("Main run");
+
+
+
+    }
+
+}
+
+
+class MyCallable implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += (i + 1);
+            System.out.println("call thread    " + sum);
+//            Thread.yield();
+
+        }
+        return sum;
+    }
+}
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("Thread run");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        while (!interrupted()) {
+            System.out.println("i am loop");
+        }
+        System.out.println("Thread end");
+    }
+}
